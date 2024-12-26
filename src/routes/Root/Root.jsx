90b7b13049d32499d14a707f66a46995a8c65ff8
misha4322 <mishnefnara@gmail.com>
2024-12-26 09:46:48 +0300

    финал2

--- src/routes/Root/Root.jsx
+++ src/routes/Root/Root.jsx
@@ -6,32 +6,7 @@ import logo from '../../assets/logo.svg'
 export const Root = () => {
   return (
     <>
-      {/* <div className={s.menu}>
 
-        <nav className={s.div}>
-          <NavLink className={({ isActive, isPending }) => isActive ? s.active : isPending ? s.loading : s.btn} to={'/'}>
-            <div className={s.qwerty}>
-              <img src={logo} alt="" /></div>
-          </NavLink>
-          <NavLink className={({ isActive, isPending }) => isActive ? s.active : isPending ? s.loading : s.btn} to={'/'}>
-            <div className={s.qwerty}> Главная</div>
-          </NavLink>
-          <NavLink className={({ isActive, isPending }) => isActive ? s.active : isPending ? s.loading : s.btn} to={'gather'}>
-            <div className={s.qwerty}>Конфигуратор</div>
-          </NavLink>
-
-          <NavLink className={({ isActive, isPending }) => isActive ? s.active : isPending ? s.loading : s.btn} to={'basket'}>
-            <div className={s.qwerty}>
-              <img src={trash} alt="" />
-
-            </div>
-          </NavLink>
-        </nav>
-
-        <div>
-          <Outlet />
-        </div>
-      </div> */}
       <header className={s.container}>
         <nav className={s.navDiv}>
         <NavLink className={({ isActive, isPending }) => isActive ? s.active : isPending ? s.loading : s.btn} to={'/'}>
